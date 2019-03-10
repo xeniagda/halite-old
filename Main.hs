af1878660c3a3a332ef9848c890a5697f6b1754b
loovjo <jonathan.loov@gmail.com>
2019-03-10 10:14:36 +0100

    Add timing

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified Data.Text as T
+import Data.Time.Clock
 
 import Interpreter
 import Code
@@ -9,33 +10,46 @@ import Value
 import Parser
 import Parse
 
-run :: Ast -> IO ()
+run :: Ast -> IO (UTCTime, UTCTime)
 run ast = do
-    putStrLn $ "Evaluating:\n" ++ apprint 0 ast
 
     let code = astToCode ast
 
-    putStrLn $ "Desugared: " ++ cpprint code
+    parseDone <- getCurrentTime
+
     let v = intoExpr code
         (mem, weaked) = weak [] v
+        (mem', evaled) = eval mem weaked
+
+    runDone <- evaled `seq` mem' `seq` getCurrentTime
+
+    putStrLn $ "Evaluating:\n" ++ apprint 0 ast
+    putStrLn $ "Desugared: " ++ cpprint code
 
     mapM_ (\(i, v) -> putStrLn $ (show i ++ ": " ++ vpprint v)) $ zip [0..] mem
     putStrLn $ "Weak: " ++ vpprint weaked
 
-    let (mem', evaled) = eval mem weaked
 
     putStrLn "Evaled memory: "
     mapM_ (\(i, v) -> putStrLn $ (show i ++ ": " ++ vpprint v)) $ zip [0..] mem
     putStrLn $ "Evaled: " ++ vpprint evaled
 
+    return (parseDone, runDone)
+
 main :: IO ()
 main = do
     inp <- T.pack <$> readFile "in.hlt"
 
+    start <- getCurrentTime
+
     case doParse parseAst inp 0 of
         Right (ast, len) ->
             if len == T.length inp
-                then run ast
+                then do
+                    (parseDone, runDone) <- run ast
+                    putStrLn $ "Parsing took: " ++ show (parseDone `diffUTCTime` start)
+                    putStrLn $ "Running took: " ++ show (runDone `diffUTCTime` parseDone)
+                    putStrLn $ "All: " ++ show (runDone `diffUTCTime` start)
                 else putStrLn "Not all parsed!"
         Left errs ->
             mapM_ (showErr 0 (T.lines inp)) errs
